e686e0d915af21bbc942bb701b6c8ce62e4ddf49
Teresa Martyny <teresamartyny@gmail.com>
2015-07-31 09:07:29 -0700

    add prime table

---
+++ solutions/prime_table/prime_grid.rb
@@ -0,0 +1,60 @@
+require_relative 'prime_assessment'
+require 'terminal-table'
+
+class PrimeGrid
+  include PrimeAssessment
+  
+  attr_reader :gridsize, :prime_list, :number, :table, :rows
+
+  def initialize(args)
+    @gridsize = args.fetch(:gridsize)
+    @prime_list = []
+    @number = 2
+    @table = Terminal::Table.new
+    @rows = []
+  end
+
+  def generate_primes_list
+    until prime_list.length == gridsize do
+      prime_list << number if is_prime?(number)
+      @number += 1
+    end
+  end
+  def add_title
+    table.title = "Primes Grid"
+  end
+  def add_header
+    rows << [""] + prime_list
+  end
+  def add_solutions
+    index = 0
+    prime_list.each do
+      next_row = []
+      next_row << prime_list[index]
+      next_row += prime_list.map{|prime| prime_list[index] * prime}
+      index += 1
+      rows << next_row
+    end
+  end
+  def add_rows
+    table.rows = rows
+  end
+  def generate_table
+    generate_primes_list
+    add_title
+    add_header
+    add_solutions
+    add_rows
+  end
+  def print_table
+    generate_table
+    puts table
+  end
+end
+
+# if ARGV.first.to_i > 0
+#   grid = PrimeGrid.new(gridsize: ARGV.first.to_i)
+#   grid.print_table
+# else
+#   raise ArgumentError, "#{ARGV.first} is not a valid grid dimention. Try again with a positive integer."
+# end
---
+++ solutions/prime_table/prime_grid_spec.rb
@@ -0,0 +1,57 @@
+require_relative 'prime_grid'
+
+describe PrimeGrid do
+  let(:grid) { PrimeGrid.new(gridsize: 10) }
+  describe "#gridsize" do
+    it "should equal 10" do
+      expect(grid.gridsize).to eq(10)
+    end
+  end
+  describe "#is_prime?" do
+    it "should return false if the number is not prime" do
+      expect(grid.is_prime?(4)).to be false
+    end
+  end
+  describe "#generate_primes_list" do
+    it "should generate an array the length of gridsize of prime numbers" do
+      expect{grid.generate_primes_list}.to change{grid.prime_list}.from([]).to([2,3,5,7,11,13,17,19,23,29])
+    end
+  end
+  describe "#table" do
+    it "should be an object" do
+      expect(grid.table).to be_instance_of Terminal::Table
+    end
+  end
+  describe "#add_title" do
+    it "should add a title to the table" do
+      expect{grid.add_title}.to change{grid.table.title}.from(nil).to("Primes Grid")
+    end
+  end
+  describe "#add_header" do
+    before {grid.generate_primes_list}
+    it "should add the prime_list to the table as the first row" do
+      expect{grid.add_header}.to change{grid.rows}.from([]).to([["",2,3,5,7,11,13,17,19,23,29]])
+    end
+  end
+  describe "#add_solutions" do
+    before {grid.generate_primes_list}
+    before {grid.add_header}
+    it "should generate the solutions rows and add them to the rows array, making the rows array contain 11 nested arrays including the header row." do
+      expect{grid.add_solutions}.to change{grid.rows.length}.from(1).to(11)
+    end
+  end
+  describe "#add_rows" do
+    before {grid.generate_primes_list}
+    before {grid.add_header}
+    before {grid.add_solutions}
+
+    it "should take the complete rows array and add those arrays to the table." do
+      expect{grid.add_rows}.to change{grid.table.rows.length}.from(0).to(11)
+    end
+  end
+  describe "#print_table" do
+    it "should print the table to STDOUT" do
+      expect{grid.print_table}.to output{table}.to_stdout
+    end
+  end
+end
